1d92507e1ddca9ab4a68429b11fc50f08c8577b9
시혁 박 <exon.indiv@gmail.com>
2020-07-13 11:33:09 +0900

    edit ico

--- Discord Profile/Discord Profile/Discord Profile.Designer.cs
+++ Discord Profile/Discord Profile/Discord Profile.Designer.cs
@@ -792,6 +792,7 @@
             this.Controls.Add(this.labToken);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
